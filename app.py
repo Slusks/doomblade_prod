82bde277b90ba8425ff0a305709b59fcfc4b296a
Slusks <samslusky@gmail.com>
2024-10-21 09:01:03 -0400

    Update Firebase Credentials

--- app.py
+++ app.py
@@ -19,20 +19,31 @@ import os
 app = Flask(__name__)
 
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, initialize_app
+
+firebase_credentials = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
 
 # Initialize the Firebase app
 # Load Firebase credentials from environment variables
-firebase_creds_json = os.getenv('FIREBASE_CREDENTIALS')
-if firebase_creds_json:
-    cred_dict = json.loads(firebase_creds_json)
-    cred = credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://doomblade-b08ea-default-rtdb.firebaseio.com/'
-    })
-else:
-    raise ValueError("Firebase credentials not set in environment variables")
 
+# Initialize Firebase app with the credentials and database URL
+
+cred = credentials.Certificate(firebase_credentials)
+initialize_app(cred, {
+    'databaseURL': 'https://doomblade-b08ea-default-rtdb.firebaseio.com/'
+})
 
 # Load data from Firebase
 def fetch_data_from_firebase(path):
